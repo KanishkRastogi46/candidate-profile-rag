0cbf8a33c042cce24b254dccfeb861a68fd3f419
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2025-03-16 02:01:06 +0530

    added authentication

--- lib/gemini.config.ts
+++ lib/gemini.config.ts
@@ -5,9 +5,6 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
     model: "gemini-2.0-flash",
-    systemInstruction: `
-    
-    `
 });
 
 export const generationConfig = {
